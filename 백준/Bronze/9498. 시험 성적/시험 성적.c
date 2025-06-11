e52794ece3f041432de03315d2c0cf6436a6f7d9
kymin12 <ym716000@naver.com>
2025-06-11 15:20:09 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	if (a >= 90) printf("A");
+	else if (a >= 80) printf("B");
+	else if (a >= 70) printf("C");
+	else if (a >= 60) printf("D");
+	else printf("F");
+}
