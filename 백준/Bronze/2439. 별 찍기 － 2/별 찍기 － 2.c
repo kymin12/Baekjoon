599e890c26982e1f57f11b171b0d31f9c0037014
kymin12 <ym716000@naver.com>
2025-06-14 23:11:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (j = n; j > i; j--)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
